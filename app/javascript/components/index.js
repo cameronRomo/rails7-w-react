856b8e56677481dad27eb3371ac815be84b54a34
Cameron Romo <cromo1225@gmail.com>
2022-11-21 10:11:55 -0700

    Add clock component

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,4 +1,6 @@
 import { render } from "react-dom";
 import h from "components/htm_create_element";
 
-render(h`<h1>Hello world from HTM</h1>`, document.getElementById("root"));
+import Clock from "components/clock";
+
+render(h`<${Clock} />`, document.getElementById("root"));
